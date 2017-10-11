ce5d7824b58dbb8a2cead271fb45b0ebf842858d
challenge <1178649872@qq.com>
2017-10-11 10:28:14 +0800

    java资料第二次提交

---
+++ java文档资料/actionScript(Flex)+red5/example/flex与js通信/html/JS与FLEX_AS实现互相调用/JS与FLEX_AS实现互相调用 - 我的技术博客 - 51CTO技术博客_files/ajs.php
@@ -0,0 +1,3 @@
+var OX_f98f2598 = '';
+OX_f98f2598 += "<"+"div style=\" color:red;font-family:Î¢ÈíÑÅºÚ \"><"+"/div><"+"div id=\'beacon_eddd3029fd\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'><"+"img src=\'//gg2.51cto.com/www/delivery/lg.php?bannerid=3687&amp;campaignid=804&amp;zoneid=460&amp;loc=1&amp;referer=http%3A%2F%2Fprogrammer.blog.51cto.com%2F2859493%2F1423553&amp;cb=eddd3029fd\' width=\'0\' height=\'0\' alt=\'\' style=\'width: 0px; height: 0px;\' /><"+"/div>\n";
+document.write(OX_f98f2598);
---
+++ java文档资料/actionScript(Flex)+red5/example/flex与js通信/html/JS与FLEX_AS实现互相调用/JS与FLEX_AS实现互相调用 - 我的技术博客 - 51CTO技术博客_files/index.php
@@ -0,0 +1,1 @@
+document.getElementById('login_status').innerHTML='<div class="login" style="padding-top:8px; text-align:right;"><a href="http://home.51cto.com/index?reback=http://programmer.blog.51cto.com/2859493/1423553" target="_self">登录</a><a href="http://ucenter.51cto.com/2015/register.php?reback=http://programmer.blog.51cto.com/2859493/1423553" target="_self">注册</a></div>';
